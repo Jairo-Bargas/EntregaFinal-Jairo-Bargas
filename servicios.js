8bdf514f3d861a5039e7d268288542bb12764442
jfbargas <jfacundobargas2@gmail.com>
2024-09-07 19:34:53 -0300

    Entrega Final JAIRO BARGAS Definitivo

--- servicios.js
+++ servicios.js
@@ -29,7 +29,7 @@ function renderizarProductos() {
     productosDiv.style.justifyContent = 'space-between'; 
     productosDiv.style.flexWrap = 'wrap';
     productosDiv.style.gap = '10px';
-    productosDiv.style.height = '60vh';
+    productosDiv.style.height = '65vh';
     productosDiv.innerHTML = ''; 
     const usuario = JSON.parse(localStorage.getItem(`usuario`));
     if (!usuario) return;
@@ -40,12 +40,19 @@ function renderizarProductos() {
         const servicios = datos;
         servicios.forEach(producto => {
             const productoDiv = document.createElement('div');
+            const imagenFondo = `../imagenes/imagen${producto.id}.jpg`
+
             productoDiv.style.backgroundColor = 'lightblue';
-            productoDiv.style.alignItems = 'end'; 
-            productoDiv.style.justifyContent = 'center';
-            productoDiv.style.width = '49%';
-            productoDiv.style.height = 'auto';
+            productoDiv.style.width = '40vh'; 
+            productoDiv.style.height = 'auto'; 
+            productoDiv.style.backgroundPosition = 'center';
+            productoDiv.style.backgroundSize = 'contain';
+            productoDiv.style.backgroundRepeat = 'no-repeat';
             productoDiv.style.display = 'flex';
+            productoDiv.style.alignItems = 'center'; 
+            productoDiv.style.justifyContent = 'center';
+            productoDiv.style.border = '1px solid #ccc';
+            productoDiv.style.backgroundImage = `url("${imagenFondo}")`
 
 
             productoDiv.innerHTML = `
@@ -53,7 +60,7 @@ function renderizarProductos() {
         <div style="margin-top: auto; display: flex; justify-content: flex-start; align-items: flex-end;">
             <span>${producto.servicio}: $${producto.price * maquina.unidadesCotizables}</span>
         </div>
-        <div style="position: absolute; bottom: 0; right: 0;">
+        <div style=" bottom: 0; right: 0;">
             <button onclick="agregarAlCarrito(${producto.id})">Añadir al Carrito</button>
         </div>
     </div>
@@ -122,6 +129,8 @@ async function calcularTotalCarrito() {
 /* FUNCIÓN PARA RENDERIZAR EL CARRITO */
 
 const renderizarCarrito = async()=> {
+    const tituloCarrito = document.getElementById("tituloCarrito")
+        tituloCarrito.style.cssText = "text-align: center; ";
     const itemscarrito = document.getElementById('itemscarrito');
     itemscarrito.innerHTML = '';
 
@@ -142,63 +151,75 @@ function eliminarCarrito(id) {
 }
 
 /* FUNCIÓN DE FINALIZAR LA COMPRA */
-function finalizarCompra(){
-    if(carrito.length === 0){
-        document.getElementById("finalizarCompra").addEventListener("click", ()=>{
-            Swal.fire("Carrito vacío");
-        })
-       
+
+    document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("finalizarCompra").addEventListener("click", finalizarCompra);
+});
+
+function finalizarCompra() {
+    
+    // Verificar si el carrito está vacío
+    if (carrito.length === 0) {
+        Swal.fire("Carrito vacío");
         return;
-    } 
+    }
+
+    // Verificar si hay fechas reservadas
+    let occupiedDates = JSON.parse(localStorage.getItem('occupiedDates')) || [];
+    if (occupiedDates.length === 0) {
+        Swal.fire({
+            icon: "warning",
+            title: "No hay fechas reservadas",
+            text: "Por favor seleccione una fecha antes de finalizar la compra.",
+        });
+        return;
+    }
+
     calcularTotalCarrito().then(total => {
-        const datosUsuarioFinal = JSON.parse(localStorage.getItem(`usuario`))
+        const datosUsuarioFinal = JSON.parse(localStorage.getItem('usuario'));
         const swalWithBootstrapButtons = Swal.mixin({
             customClass: {
-              confirmButton: "btn btn-success",
-              cancelButton: "btn btn-danger"
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
             },
             buttonsStyling: false
-          });
-          swalWithBootstrapButtons.fire({
+        });
+
+        swalWithBootstrapButtons.fire({
             title: "DESEA FINALIZAR LA COMPRA?",
             text: "Por favor controle correctamente su pedido",
-
             showCancelButton: true,
             confirmButtonText: "Si, finalizar",
             cancelButtonText: "No, cancelar",
             reverseButtons: true
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
-              swalWithBootstrapButtons.fire({
-                title: `COMPRA FINALIZADA`,
-                text: `Nombre y Apellido: ${datosUsuarioFinal.nombre.toUpperCase()} ${datosUsuarioFinal.apellido.toUpperCase()} 
-                
-                
-                Precio: $${total}\nMuchas gracias por su compra.`,
-                icon: "success"
-              });
-            } else if (
-              /* Read more about handling dismissals below */
-              result.dismiss === Swal.DismissReason.cancel
-            ) {
-              swalWithBootstrapButtons.fire({
-                title: "CANCELADO",
-              });
+                swalWithBootstrapButtons.fire({
+                    title: `COMPRA FINALIZADA`,
+                    html: `Nombre y Apellido: ${datosUsuarioFinal.nombre.toUpperCase()} ${datosUsuarioFinal.apellido.toUpperCase()}<br> Precio: $${total}<br>Fecha: ${occupiedDates}<br>Muchas gracias por su compra.`,
+                    icon: "success"
+                });
+
+                // Limpiar el carrito y actualizar el localStorage
+                carrito = [];
+                localStorage.setItem('carrito', JSON.stringify(carrito));
+                occupiedDates = [];
+                localStorage.setItem(`occupiedDates`, JSON.stringify(occupiedDates))
+                renderizarCarrito();
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                swalWithBootstrapButtons.fire({
+                    title: "CANCELADO",
+                });
             }
-          });
-        carrito = []
-        localStorage.setItem(`carrito`, JSON.stringify(carrito))
-        renderizarCarrito()
-    }).catch(error =>{
+        });
+    }).catch(error => {
         Swal.fire({
             icon: "error",
             title: "Error",
             text: "Algo salió mal, vuelva a intentarlo",
-          });
-    })
+        });
+    });
 }
-
-
 document.addEventListener(`DOMContentLoaded`, function() {
     const botonFinalizar = document.getElementById("finalizarCompra");
     if (botonFinalizar){
@@ -226,6 +247,84 @@ function bienvenidaUsuario(){
         }
     })
 }
+
+/* Se agrega la función de un calendario mediante librerías */
+// Inicializa flatpickr con la configuración deseada
+const calendar = flatpickr("#calendar", {
+    onChange: function(selectedDates, dateStr, instance) {
+        handleDateSelection(dateStr);
+    },
+    disable: getOccupiedDates(),
+    onReady: function(selectedDates, dateStr, instance) {
+        applyStyles();
+    }
+});
+
+// Función para aplicar estilos CSS directamente en JavaScript
+function applyStyles() {
+    const inputField = document.querySelector(".flatpickr-input");
+    const calendarContainer = document.querySelector(".flatpickr-calendar");
+    
+    if (inputField) {
+        inputField.style.padding = "5px 10px";
+        inputField.style.margin = "20px 10px 10px 20px";
+        inputField.style.fontSize = "15px"; 
+        inputField.style.width = "15%"; 
+        inputField.style.maxWidth = "400px"; 
+    }
+
+    if (calendarContainer) {
+        calendarContainer.style.fontSize = "14px"; 
+    }
+}
+
+// Llama a applyStyles después de que Flatpickr haya inicializado el campo
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(applyStyles, 0);
+});
+
+function handleDateSelection(date) {
+    let occupiedDates = JSON.parse(localStorage.getItem('occupiedDates')) || [];
+
+    if (occupiedDates.includes(date)) {
+        // Si la fecha ya está ocupada, eliminarla de la lista
+        occupiedDates = occupiedDates.filter(d => d !== date);
+        localStorage.setItem('occupiedDates', JSON.stringify(occupiedDates));
+        Swal.fire({
+            icon: "info",
+            title: "Fecha desmarcada",
+            text: `La fecha ${date} ha sido desmarcada.`,
+        });
+    } else {
+        // Agregar la nueva fecha seleccionada
+        if (occupiedDates.length >= 1) {
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: `Solo puede reservar una fecha.`,
+            });
+            return; // No agregar más fechas si ya hay una reservada
+        }
+        occupiedDates.push(date);
+        localStorage.setItem('occupiedDates', JSON.stringify(occupiedDates));
+        Swal.fire({
+            icon: "success",
+            title: "Fecha reservada",
+            text: `La fecha seleccionada es ${date}`,
+        });
+    }
+
+    // Actualiza la lista de fechas deshabilitadas en el calendario
+    calendar.set("disable", getOccupiedDates());
+}
+
+function getOccupiedDates() {
+    let occupiedDates = JSON.parse(localStorage.getItem('occupiedDates')) || [];
+    return occupiedDates.map(date => new Date(date));
+}
+
+/* FUNCIONES QUE VERIFICAN SI OTRO USUARIO ESTÁ INGRESANDO PARA REINICIAR DATOS */
+
 
 const run=()=>{
     bienvenidaUsuario()
